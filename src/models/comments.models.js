0123e0f1a092a3fdb4f17fbdf83514896a0bb0db
Sumeet Raina <sumeet@Mac.broadband>
2025-05-01 15:41:18 +0100

    Delete comment by id

---
+++ src/models/comments.models.js
@@ -0,0 +1,9 @@
+const db = require("../../db/connection");
+
+exports.deleteCommentBy = (comment_id) => {
+  const result = db.query("DELETE FROM comments WHERE comment_id= $1", [
+    comment_id,
+  ]);
+  console.log(result, "mod");
+  return result;
+};
