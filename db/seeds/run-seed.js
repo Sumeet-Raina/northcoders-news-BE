01942c967c7fa6971daeee36f2d0e52e5e070c9f
Sumeet Raina <sumeet@Mac.broadband>
2025-05-02 11:04:57 +0100

    Fixed db setup

--- db/seeds/run-seed.js
+++ db/seeds/run-seed.js
@@ -4,7 +4,10 @@ const seed = require("./seed.js");
 const db = require("../connection.js");
 
 const runSeed = () => {
-  if (process.env.NODE_ENV === "development") {
+  if (
+    process.env.NODE_ENV === "development" ||
+    process.env.NODE_ENV === "production"
+  ) {
     return seed(devData).then(() => db.end());
   } else {
     return seed(testData).then(() => db.end());
