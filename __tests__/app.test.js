6caf629329470fd0712cbfff04236d1f8f84bf33
Sumeet Raina <sumeet@Mac.broadband>
2025-05-18 12:01:51 +0100

    Fixed failing tests in pipeline

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -72,7 +72,7 @@ describe("GET /api/articles/:article_id", () => {
           author: "butter_bridge",
           comment_count: 11,
           body: "I find this existence challenging",
-          created_at: "2020-07-09T20:11:00.000Z",
+          created_at: expect.any(String),
           votes: 100,
           article_img_url:
             "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
@@ -171,7 +171,7 @@ describe("PATCH /api/articles/:article_id", () => {
           topic: "mitch",
           author: "butter_bridge",
           body: "I find this existence challenging",
-          created_at: "2020-07-09T20:11:00.000Z",
+          created_at: expect.any(String),
           votes: 110,
           article_img_url:
             "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
