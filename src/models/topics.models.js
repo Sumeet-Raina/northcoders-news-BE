9e45fe74d37ffdb8baf6f55f07c6951d5d8ce872
Sumeet Raina <sumeet@Mac.broadband>
2025-05-19 17:55:17 +0100

    Refactored code to use Router

--- src/models/topics.models.js
+++ src/models/topics.models.js
@@ -1,6 +1,8 @@
 const db = require("../../db/connection");
 
 exports.selectTopics = async () => {
-  const topics = await db.query("SELECT slug, description FROM topics;");
+  const topics = await db.query(
+    "SELECT slug, description FROM topics OFFSET 0 LIMIT 10"
+  );
   return topics.rows;
 };
