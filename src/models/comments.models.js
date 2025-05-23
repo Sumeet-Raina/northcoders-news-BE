261ded2402a67b9ef127bcbab68993153c0efd05
Sumeet Raina <sumeet@Mac.broadband>
2025-05-23 21:14:23 +0100

    Patch comment by id

--- src/models/comments.models.js
+++ src/models/comments.models.js
@@ -6,3 +6,12 @@ exports.deleteCommentBy = (comment_id) => {
   ]);
   return result;
 };
+
+exports.updateCommentById = async (comment_id, inc_votes) => {
+  const { rows } = await db.query(
+    "UPDATE comments SET votes = votes + $1 WHERE comment_id = $2 returning *;",
+    [inc_votes, comment_id]
+  );
+  console.log(rows[0], "<===model");
+  return rows[0];
+};
