d4d694d9fc5ea80293e7c4e474a898851390f037
Sumeet Raina <sumeet@Mac.broadband>
2025-05-24 15:31:45 +0100

    Create new topic

--- src/models/topics.models.js
+++ src/models/topics.models.js
@@ -4,3 +4,12 @@ exports.selectTopics = async () => {
   const topics = await db.query(`SELECT slug, description FROM topics`);
   return topics.rows;
 };
+
+exports.createTopic = async (slug, description) => {
+  const result = await db.query(
+    `INSERT INTO TOPICS (slug, description) VALUES ($1, $2) RETURNING *;`,
+    [slug, description]
+  );
+
+  return result.rows[0];
+};
